ab127b56aa0bd7358fd1f3f230230b623eb25957
Febin Tom Mathews <febintm2005@gmail.com>
2025-10-21 17:22:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const backend = "http://localhost:5000";
+const backend = "https://hostel-managament-1.onrender.com";
+
 
 // ---------------- Load Reports ----------------
 document.addEventListener("DOMContentLoaded", async () => {
@@ -252,4 +253,5 @@ if (allocationsTable) {
 
     loadAllocations();
 }
+
 
